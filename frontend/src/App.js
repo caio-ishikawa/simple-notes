ba10a492d371607d83c7bddfabbf9f04d58309ed
caio-ishikawa <caioarbishikawa@gmail.com>
2021-10-22 18:47:53 +0100

    code editor implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.css'; import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownKeymap, markdownLanguage } from '@codemirror/lang-markdown';
+import 'codemirror/theme/cobalt.css';
 
 function App() {
   return (
     <div className="App">
       <p>Test</p>
+      <CodeMirror
+      extensions={[
+        markdown({ base: markdownLanguage })
+      ]}
+      options={{
+        theme: 'cobalt',
+        lineNumers: false
+      }}
+      />
     </div>
   );
 }
