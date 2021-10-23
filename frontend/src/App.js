76752b9f649cdad5f1992b2f7ec36b70006cf219
caio ishikawa <caioarbishikawa@gmail.com>
2021-10-23 20:48:56 +0100

    monaco editor implementation + VIM keybinds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,46 @@
-import './App.css'; import CodeMirror from '@uiw/react-codemirror';
-import { markdown, markdownKeymap, markdownLanguage } from '@codemirror/lang-markdown';
-import 'codemirror/theme/cobalt.css';
+import './App.css'; 
+import Editor from '@monaco-editor/react';
 
 function App() {
+
+  function handleEditorDidMount(editor, monaco) {
+    // Setup key bindings
+    editor.addAction({
+      // Unique identifier of the contributed action
+      id: "some-unique-id",
+      // A label of the action that will be presented to the user
+      label: "Some label!",
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S],
+      // Method that will be executed when the action is triggered.
+      run: function (editor) {
+        return null;
+      }
+    });
+
+    // Setup monaco-vim
+    window.require.config({
+      paths: {
+        "monaco-vim": "https://unpkg.com/monaco-vim/dist/monaco-vim"
+      }
+    });
+
+    window.require(["monaco-vim"], function (MonacoVim) {
+      const statusNode = document.querySelector(".status-node");
+      MonacoVim.initVimMode(editor, statusNode);
+    });
+  }
+
   return (
-    <div className="App">
-      <p>Test</p>
-      <CodeMirror
-      extensions={[
-        markdown({ base: markdownLanguage })
-      ]}
-      options={{
-        theme: 'cobalt',
-        lineNumers: false
-      }}
-      />
-    </div>
+    <div>
+    <Editor
+      height="90vh"
+      language="markdown"
+      wrapperClassName="something"
+      onMount={handleEditorDidMount}
+      theme="vs-dark"
+    />
+    <code className="status-node"></code>
+  </div>
   );
 }
 
